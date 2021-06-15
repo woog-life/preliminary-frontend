e766ad96329239bc0639fd51e9a3f1d1750052cf
Torben Carstens <github@carstens.tech>
2021-06-15 19:42:09 +0200

    Don't show bookings for alster/cuxhaven (no bookings necessary)

--- src/App.fs
+++ src/App.fs
@@ -313,10 +313,13 @@ let displayLake model dispatch =
             ]
             (match model.Lake with
              | Some lake ->
-                 div [] [
-                     displayEventCollapseButton
-                     displayEvents lake.Events
-                 ]
+                 if List.contains lake.Uuid ["d074654c-dedd-46c3-8042-af55c93c910e"; "55e5f52a-2de8-458a-828f-3c043ef458d9"] then
+                    span [] []
+                 else
+                     div [] [
+                         displayEventCollapseButton
+                         displayEvents lake.Events
+                     ]
              | None -> span [] [])
         ]
     ]
