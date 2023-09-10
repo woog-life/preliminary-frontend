d4a4ee754060e22af8509ef1d97cb742b0a11cc7
Torben Carstens <git@carstens.tech>
2023-09-10 17:19:25 +0200

    Add RFC links to `Accept-Language` value regex

--- src/helper.cr
+++ src/helper.cr
@@ -5,6 +5,11 @@ def get_country_code_from_header(accept_language_value : String?) : String?
   end
 
   language : String = accept_language_value.as(String)
+
+  # see the following two RFCs for the specification: 
+  # https://www.rfc-editor.org/rfc/rfc3282 (Content Language Headers)
+  # https://www.rfc-editor.org/rfc/rfc3066 (Tags for the Identification of Languages)
+  # note that while rfc3066 is obsoleted by rfc4646 + rfc4647 this is not reflected in RFC3282, thus we're using the definition from rfc3066
   header_value_regex = /^((?<primary>\*|([A-Z]{1,8}))((?<!\*)-(?<subtag>[A-Z0-9]{1,8}))?)(;q=(?<quality>1|0|0.[0-9]{1,3}))?$/i
   if value = language.match(header_value_regex)
     value["subtag"]
